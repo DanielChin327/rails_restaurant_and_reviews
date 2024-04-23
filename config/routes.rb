4b9108edaa40b4eefc513700937de437ddade699
Daniel Chin <DanChin327@gmail.com>
2024-04-23 22:45:14 +0900

    updated reviews and htmls

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
     # collection vs member -> many vs one.
   end
-
+  resources :reviews, only: [:destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
